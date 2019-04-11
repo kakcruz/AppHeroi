f194dbf43369aed358b6cc7e21dfff9762f00256
manager <karolinecsantos@hotmail.com>
2019-04-10 22:13:07 -0300

    appHeroi

---
+++ app/src/main/java/br/com/etecia/appheroi/MenuPersonagem_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.appheroi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MenuPersonagem_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menupersonagem_layout);
+    }
+}
